57f1e5fc608f58ebbb5835cfe7b8e542bca93421
Damian Dudycz <ddudycz@mobitouch.net>
2020-06-11 23:30:45 +0200

    More MIME types

--- Sources/WebService/Helper types/MIME Types/ContentType.swift
+++ Sources/WebService/Helper types/MIME Types/ContentType.swift
@@ -11,19 +11,23 @@ public extension URLRequest {
     
     enum ContentType {
         
-        // TODO: More types.
-        // TODO: Split application like image into container
+        case multipartFormData(boundary: Boundary)
         case text             (_ type: TextType = .plain, encoding: String.Encoding = .utf8)
         case application      (_ type: ApplicationType = .octetStream, encoding: String.Encoding = .utf8)
-        case multipartFormData(boundary: Boundary)
         case image            (_ type: ImageType)
-        
+        case audio            (_ type: AudioType)
+        case video            (_ type: VideoType)
+        case model            (_ type: ModelType)
+
         var string: String {
             switch self {
-            case let .text             (type, encoding): return "text/\(type.rawValue);charset=\(encoding.httpName)"
-            case let .application      (type, encoding): return "application/\(type.rawValue);charset=\(encoding.httpName)"
             case let .multipartFormData(boundary):       return "multipart/form-data;boundary=\(boundary)"
+            case let .application      (type, encoding): return "application/\(type.rawValue);charset=\(encoding.httpName)"
+            case let .text             (type, encoding): return "text/\(type.rawValue);charset=\(encoding.httpName)"
             case let .image            (type):           return "image/\(type.rawValue)"
+            case let .audio            (type):           return "audio/\(type.rawValue)"
+            case let .video            (type):           return "video/\(type.rawValue)"
+            case let .model            (type):           return "model/\(type.rawValue)"
             }
         }
         
---
+++ Sources/WebService/Helper types/MIME Types/Media types/VideoType.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Home Dudycz on 11/06/2020.
+//
+
+import Foundation
+
+public extension URLRequest {
+    
+    enum VideoType: String {
+        case mp4, h264 = "H264", h265 = "H265", mpeg4Generic = "mpeg4-generic"
+    }
+    
+}
