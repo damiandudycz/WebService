0cab8d4ed5ddffa7e0246e27d9e049232450ba99
Damian Dudycz <ddudycz@mobitouch.net>
2020-06-11 16:17:19 +0200

    Encoding multipart

--- Sources/WebService/Helper types/MultipartFormEncoder.swift
+++ Sources/WebService/Helper types/MultipartFormEncoder.swift
@@ -30,15 +30,15 @@ public struct MultipartFormEncoder: BodyEncoder {
         switch parameters {
         case let data as Data:
             // TODO: Prehaps we should pass type in Encoder initializer instead.
-            return buildBody(data, name: "file", filename: UUID().uuidString, type: .applicationOctetStream)
+            return buildFormBody(data, name: "file", filename: UUID().uuidString, type: .applicationOctetStream)
         case let image as UIImage:
             guard let data = image.jpegData(compressionQuality: 1.0) else {
                 throw EncodingError.conversionFailed
             }
-            return buildBody(data, name: "image", filename: "\(UUID()).jpg", type: .image(.jpeg))
+            return buildFormBody(data, name: "image", filename: "\(UUID()).jpg", type: .image(.jpeg))
         case let json as DictionaryRepresentable:
             let dictionary = try json.dictionary()
-            return buildBody(dictionary)
+            return buildFormBody(dictionary)
         default:
             throw EncodingError.unsupportedParametersType
         }
@@ -50,10 +50,15 @@ public struct MultipartFormEncoder: BodyEncoder {
 private extension MultipartFormEncoder {
     
     // Form fragments adding
-    func insert(_ data: Data, name: String, filename: String, type: URLRequest.ContentType, to body: inout Data) {
+    func insert(_ data: Data, name: String, filename: String?, type: URLRequest.ContentType, to body: inout Data) {
         var part = Data()
+        // TODO: Extension for string -> Data conversion.
         part.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-        part.append("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
+        part.append("Content-Disposition: form-data; name=\"\(name)\"".data(using: .utf8)!)
+        if let filename = filename {
+            part.append("; filename=\"\(filename)\"".data(using: .utf8)!)
+        }
+        part.append("\r\n".data(using: .utf8)!)
         part.append("Content-Type: \(type.string)\r\n\r\n".data(using: .utf8)!)
         part.append(data)
         body.append(part)
@@ -66,19 +71,18 @@ private extension MultipartFormEncoder {
 
     // MARK: - Adding various data types
     
-    func buildBody(_ data: Data, name: String, filename: String, type: URLRequest.ContentType) -> Data {
+    func buildFormBody(_ data: Data, name: String, filename: String?, type: URLRequest.ContentType) -> Data {
         var form = Data()
         insert(data, name: name, filename: filename, type: type, to: &form)
         finishForm(&form)
         return form
     }
 
-    func buildBody(_ dictionary: [String : CustomStringConvertible]) -> Data {
+    func buildFormBody(_ dictionary: [String : CustomStringConvertible]) -> Data {
         var form = Data()
         dictionary.forEach { (key, value) in
             let data = value.description.data(using: .utf8)!
-            // TODO: Remove filename and type if needed.
-            insert(data, name: key, filename: key, type: .textPlain, to: &form)
+            insert(data, name: key, filename: nil, type: .textPlain, to: &form)
         }
         finishForm(&form)
         return form
