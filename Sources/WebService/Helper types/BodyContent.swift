b7b240e75574ce3d2bb96706f4cfada839793670
Damian Dudycz <ddudycz@mobitouch.net>
2020-06-11 18:40:05 +0200

    BodyContentMaker Protocol

--- Sources/WebService/Helper types/BodyContent.swift
+++ Sources/WebService/Helper types/BodyContent.swift
@@ -7,7 +7,30 @@
 
 import Foundation
 
-public typealias BodyContent<Parameters: Encodable, Encoder: BodyEncoder> = (
-    parameters: Parameters,
-    encoder:    Encoder
-)
+public protocol BodyContentMaker {
+    func prepareBody() throws -> Data?
+}
+
+public struct BodyContent<Parameters: Encodable, Encoder: BodyEncoder>: BodyContentMaker {
+    
+    public let parameters: Parameters
+    public let encoder:    Encoder
+    
+    public init(_ parameters: Parameters, _ encoder: Encoder) {
+        self.parameters = parameters
+        self.encoder = encoder
+    }
+    
+    public func prepareBody() throws -> Data? {
+        try encoder.buildBody(parameters)
+    }
+
+}
+//
+//public struct BodyForm: BodyContentMaker {
+//    
+//    public func prepareBody() throws -> Data? {
+//        
+//    }
+//    
+//}
